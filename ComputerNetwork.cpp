0c24e1148912f6e8db63c8850263113cb0761b3c
Igor <gladkovigor2004@gmail.com>
2024-10-01 01:33:27 +0300

    version 0.2.1

--- ComputerNetwork.cpp
+++ ComputerNetwork.cpp
@@ -20,34 +20,28 @@ int main() {
         cout << " [2] Игорь Гладков {11, 5, 3, 4, 2}\n";
         cout << " [3] Никита Ромашко {10, 3, 4, 3, 3}\n";
         cout << " [4] Георгий Золоев {7, 6, 3, 2, 3}\n";
- //       cout << " [5] Тестовый вариант\n";
         cout << " [0] Выход из программы\n";
 
- //       int b = a.checking_input(5);
         int b = a.checking_input(4);
 
         switch (b) {
 
         case 1:
-            //AlexeyShihalev.Print_matrix_bandwidth();
             AlexeyShihalev.printInfo();
             AlexeyShihalev.menu();
             break;
 
         case 2: 
-            //IgorGladkov.Print_matrix_bandwidth();
             IgorGladkov.printInfo();
             IgorGladkov.menu();
             break;
 
         case 3:
-            //NikitaRomashko.Print_matrix_bandwidth();
             NikitaRomashko.printInfo();
             NikitaRomashko.menu();
             break;
 
         case 4:
-            //GeorgyZoloev.Print_matrix_bandwidth();
             GeorgyZoloev.printInfo();
             GeorgyZoloev.menu();
             break;
@@ -63,9 +57,4 @@ int main() {
         }
 
     }
-
-
-    //a.Print_matrix_bandwidth();
-    //a.Dijkstra_algorythm({ 0,5,3 });
-
 }
--- Network.h
+++ Network.h
@@ -16,32 +16,18 @@ class Network
 	int BANDWIDTH_IN_GROUP; //пропускна€ способность внутри группы
 	int BANDWIDTH_BETWEEN_GROUP; //пропускна€ способность между группами
 
-
-
-	//int count_groups; 
 	int count_commutators;
 	int count_nodes;
 	int count_vertex; //количество всех вершин (узлы + коммутаторы)
-	//int bandwidth_in_group;
-	//int bandwidth_between_group;
 	vector<vector<int>> messages;
 	vector<vector<string>> status;
 	vector<vector<int>> path;
 	vector<int> reserv;//сколько зарезервировано на шаге1 в каждом сообщении
 	vector<int> reminder; //сколько осталось передать от одной вершины к другой
-
-	
-	//vector<vector<int>> matrix_adj; //ћатрица смежности
-	//vector<vector<int>> matrix_wght; //ћатрица весов
-	//vector<int> vertex_degree; //степени вершин
 	vector<vector<int>> matrix_bandwidth; //ћатрица пропусных способностей
-	//vector<vector<int>> matrix_cost; //ћатрица стоимости
 	vector<vector<int>> matrix_load; //ћатрица загруженности
 
-
 	void print_matrix(string str, vector<vector<int>> matr);
-	
-
 	bool CheckBoolVector(vector<bool>);
 	int Min_Index(vector<int>, vector<bool>, int cost);
 	bool Can_Make_Route(vector<int> message);
